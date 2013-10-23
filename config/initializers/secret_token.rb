908254fc20ddbebac7471880bb3c5c7e5bb19810
Jorge Martinez <jorge.martinez21@live.cl>
2013-10-23 15:36:50 -0300

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proyectoswii::Application.config.secret_token = 'ce46b1953c6572cfcbb718cf5634b2e043b0e3d2007362d2b30474459524d5ae6d003f0eb6365312242428500be75cf9833cba9bde68d02299c1368750466060'
